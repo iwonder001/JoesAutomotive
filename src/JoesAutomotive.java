ad1b8bb45b0f13779032dc700035b57a9177162a
Kristen <kcarethers@gmail.com>
2017-01-04 14:51:15 -0500

    Initial Commit

---
+++ src/JoesAutomotive.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+
+public class JoesAutomotive extends JFrame {
+
+	
+		private JLabel message;
+		private JCheckBox oilChange;
+		private JCheckBox lubeJob;
+		private JCheckBox radiatorFlush;
+		private JCheckBox transmissionFlush;
+		private JCheckBox inspection;
+		private JCheckBox mufflerReplacement;
+		private JCheckBox tireRotation;
+		
+		public JoesAutomotive(){
+			//Create text for the title bar
+			setTitle("Joe's Automotive");
+			setSize(500, 500);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//Create a label
+			message
+		}
+}
